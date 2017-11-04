5401cfc15f9dd7f7f2190f27e50e4283470d4a53
emperor-fa <1040477385@qq.com>
2017-11-05 00:51:51 +0800

    同学录添加同学

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace classmate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Formadd fadd = new Formadd();
+            fadd.ShowDialog();
+        }
+
+        private void btSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
