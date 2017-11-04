747539e72f0f342f506ecf4078c17021a5a8aa54
lzping <lzping0000@163.com>
2017-11-05 02:13:48 +0800

    classbook_Alpha

---
+++ classmatebook_Alpha/吴伟根/frmClassmateInformation.designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Tongxuelu
+{
+    partial class frmClassmateInformation
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Individual_language = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.qq = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.mailbox = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.WeChat = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Telephone = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.HomeAddress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.Individual_language);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.qq);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.mailbox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.WeChat);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.Telephone);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.HomeAddress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.name);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(497, 307);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "具体信息";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "删除此人";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "修改信息";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Individual_language
+            // 
+            this.Individual_language.AutoSize = true;
+            this.Individual_language.Location = new System.Drawing.Point(122, 200);
+            this.Individual_language.Name = "Individual_language";
+            this.Individual_language.Size = new System.Drawing.Size(0, 12);
+            this.Individual_language.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(42, 200);
+            this.label9.Name = "label9";
+            this.label9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label9.Size = new System.Drawing.Size(65, 12);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "个性语言：";
+            // 
+            // qq
+            // 
+            this.qq.AutoSize = true;
+            this.qq.Location = new System.Drawing.Point(89, 154);
+            this.qq.Name = "qq";
+            this.qq.Size = new System.Drawing.Size(41, 12);
+            this.qq.TabIndex = 11;
+            this.qq.Text = "label3";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(42, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "企鹅：";
+            // 
+            // mailbox
+            // 
+            this.mailbox.AutoSize = true;
+            this.mailbox.Location = new System.Drawing.Point(336, 92);
+            this.mailbox.Name = "mailbox";
+            this.mailbox.Size = new System.Drawing.Size(41, 12);
+            this.mailbox.TabIndex = 9;
+            this.mailbox.Text = "label3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(289, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "邮箱：";
+            // 
+            // WeChat
+            // 
+            this.WeChat.AutoSize = true;
+            this.WeChat.Location = new System.Drawing.Point(89, 92);
+            this.WeChat.Name = "WeChat";
+            this.WeChat.Size = new System.Drawing.Size(41, 12);
+            this.WeChat.TabIndex = 7;
+            this.WeChat.Text = "label3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "微信：";
+            // 
+            // Telephone
+            // 
+            this.Telephone.AutoSize = true;
+            this.Telephone.Location = new System.Drawing.Point(336, 36);
+            this.Telephone.Name = "Telephone";
+            this.Telephone.Size = new System.Drawing.Size(41, 12);
+            this.Telephone.TabIndex = 5;
+            this.Telephone.Text = "label3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "电话：";
+            // 
+            // HomeAddress
+            // 
+            this.HomeAddress.AutoSize = true;
+            this.HomeAddress.Location = new System.Drawing.Point(336, 154);
+            this.HomeAddress.Name = "HomeAddress";
+            this.HomeAddress.Size = new System.Drawing.Size(41, 12);
+            this.HomeAddress.TabIndex = 3;
+            this.HomeAddress.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "家庭地址：";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(89, 36);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(41, 12);
+            this.name.TabIndex = 1;
+            this.name.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "姓名：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(195, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "同学个人信息";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 356);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "同学信息";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label HomeAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Individual_language;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label qq;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label mailbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label WeChat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Telephone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
